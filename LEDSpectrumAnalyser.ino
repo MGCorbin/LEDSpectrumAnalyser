70468f06d05d20109e843275f9675bec749f5d4b
Marcus Corbin <mgc1g18@soton.ac.uk>
2021-02-08 16:34:01 +0000

    Functions added to control LEDs

--- LEDSpectrumAnalyser.ino
+++ LEDSpectrumAnalyser.ino
@@ -19,9 +19,9 @@
 CRGB leds[NUM_LEDS];
 typedef struct
 {
-    int hue;
-    int sat;
-    int val;
+    uint8_t hue;
+    uint8_t sat;
+    uint8_t val;
     int nled;
     bool active;
 } led_t;
@@ -50,4 +50,78 @@ void loop()
     leds[0] = CRGB::Red;
     delay(100);
     leds[0] = CRGB::Black;
+}
+
+void updateLEDs()
+{
+    for(int i=0; i<COLUMN; i++)
+    {
+        for(int j=0; j<ROWS; j++)
+        {
+            if(ledColours[i][j].active)
+            {
+                leds[ledColours[i][j].nled] = CHSV(ledColours[i][j].hue, ledColours[i][j].sat, ledColours[i][j].val);
+            }
+            else
+            {
+                leds[ledColours[i][j].nled] = CRGB::Black;
+            }
+        }
+    }
+}
+
+void full_column()
+{
+    int level = 0;
+
+    for(int i=0; i<COLUMN; i++)
+    {
+        level = 5; /* fill in with normalised amplitude data */
+        for(int j=0; j<ROWS; j++)
+        {
+            if(j <= level)
+            {
+                ledColours[i][j].active = true;
+            }
+            else
+            {
+                ledColours[i][j].active = false;
+            }
+        }
+    }
+}
+
+void dot_column()
+{
+    int level = 0;
+
+    for(int i=0; i<COLUMN; i++)
+    {
+        level = 5; /* fill in with normalised amplitude data */
+        for(int j=0; j<ROWS; j++)
+        {
+            if(j == level)
+            {
+                ledColours[i][j].active = true;
+            }
+            else
+            {
+                ledColours[i][j].active = false;
+            }
+        }
+    }
+}
+
+void set_hsv_colour(uint8_t h, uint8_t s, uint8_t v)
+{
+        for(int i=0; i<COLUMN; i++)
+    {
+        level = 5; /* fill in with normalised amplitude data */
+        for(int j=0; j<ROWS; j++)
+        {
+            ledColours[i][j].hue = h;
+            ledColours[i][j].sat = s;
+            ledColours[i][j].val = v;
+        }
+    }
 }
